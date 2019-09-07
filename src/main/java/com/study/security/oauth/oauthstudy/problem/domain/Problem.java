1b80b3b827367e9be1691849a25f7d0cd78eae16
nokchax <nokchax@gmail.com>
2019-09-07 22:16:07 +0900

    feat : create problem domain

---
+++ src/main/java/com/study/security/oauth/oauthstudy/problem/domain/Problem.java
@@ -0,0 +1,11 @@
+package com.study.security.oauth.oauthstudy.problem.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Problem {
+    @Id
+    private Long id;
+
+}
